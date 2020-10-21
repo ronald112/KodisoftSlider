5287343d479e20b871f4c168bdf5d31f3aebf3e2
ronald112 <ronald11211@gmail.com>
2020-10-21 18:39:50 +0300

    Parent UIElement - very important

--- Slider/slider_class.cs
+++ Slider/slider_class.cs
@@ -22,16 +22,30 @@ namespace slider_class
         public String m_switchText { get; set; }
         public Vector4 m_margins { get; set; }
 
-        private HorizontalAlignment m_ha;
-        private VerticalAlignment m_va;
         private Compositor m_page_compositor;
         private ContainerVisual visuals;
         private UIElement m_page;
+        private CompositionLinearGradientBrush linearGradientBrush;
+        private float theMileOfSwitch;
+        //-------------------------------------------
 
-        public MyCustomSlider(HorizontalAlignment ha, VerticalAlignment va, UIElement page)
+        private Vector3KeyFrameAnimation _leftoffsetAnimation;
+        private Vector3KeyFrameAnimation _rightoffsetAnimation;
+
+        private CompositionRadialGradientBrush circleGradientBrush;
+
+        private CompositionColorGradientStop ColorStop1;
+        private ColorKeyFrameAnimation color1Animation;
+        private ColorKeyFrameAnimation color2Animation;
+
+        private Vector3 pointerPosition;
+        private ShapeVisual shapeVisualElipse;
+        private ShapeVisual shapeVisualCircle;
+        private ShapeVisual shapeVisualShadow;
+        private bool isPressed = false;
+
+        public MyCustomSlider(UIElement page)
         {
-            m_ha = ha;
-            m_va = va;
             m_page = page;
             m_page_compositor = ElementCompositionPreview.GetElementVisual(m_page).Compositor; ;
             m_margins = new Vector4(0, 0, 0, 0);
@@ -42,42 +56,134 @@ namespace slider_class
             createCanvas();
         }
 
-        private void createCanvas()
+        private void CreateMainElipse()
         {
-            /*
-            m_canvas = new Canvas();
-            m_canvas.HorizontalAlignment = m_ha;
-            m_canvas.VerticalAlignment = m_va;
-            m_canvas.Margin = new Thickness(0, 0, 0, 0);
-            m_canvas.Width = 150;
-            m_canvas.Height = 50;
-            */
-            visuals = m_page_compositor.CreateContainerVisual();
-            ElementCompositionPreview.SetElementChildVisual(m_page, visuals);
-
             CompositionRoundedRectangleGeometry roundedRectangle = m_page_compositor.CreateRoundedRectangleGeometry();
             roundedRectangle.Size = new Vector2(150, 50);
             roundedRectangle.CornerRadius = new Vector2(25, 25);
             CompositionSpriteShape compositionSpriteShape = m_page_compositor.CreateSpriteShape(roundedRectangle);
-            CompositionLinearGradientBrush linearGradientBrush = m_page_compositor.CreateLinearGradientBrush();
+            linearGradientBrush = m_page_compositor.CreateLinearGradientBrush();
             linearGradientBrush.StartPoint = new Vector2(0, 1);
             linearGradientBrush.EndPoint = new Vector2(0f, 1);
             linearGradientBrush.ColorStops.Insert(0, m_page_compositor.CreateColorGradientStop(0f, m_backgroundColor));
             linearGradientBrush.ColorStops.Insert(1, m_page_compositor.CreateColorGradientStop(0.5f, m_waveColor));
             linearGradientBrush.ColorStops.Insert(2, m_page_compositor.CreateColorGradientStop(1f, m_backgroundColor));
             compositionSpriteShape.FillBrush = linearGradientBrush;
-            ShapeVisual shapeVisualElipse = m_page_compositor.CreateShapeVisual();
+            shapeVisualElipse = m_page_compositor.CreateShapeVisual();
             shapeVisualElipse.Size = new Vector2(150, 50);
             shapeVisualElipse.Shapes.Add(compositionSpriteShape);
+        }
 
-            //m_canvas.Background = new SolidColorBrush(Colors.Aqua);
+        private void CreateTopCircleButtonShadow()
+        {
+            CompositionRoundedRectangleGeometry circleShaowGeometry = m_page_compositor.CreateRoundedRectangleGeometry();
+            circleShaowGeometry.Size = new Vector2(50, 50);
+            circleShaowGeometry.CornerRadius = new Vector2(25, 25);
+            CompositionSpriteShape compositionSpriteShapeShadow = m_page_compositor.CreateSpriteShape(circleShaowGeometry);
+            CompositionRadialGradientBrush compositionLinearGradientShadowBrush = m_page_compositor.CreateRadialGradientBrush();
+            compositionLinearGradientShadowBrush.ColorStops.Insert(0, m_page_compositor.CreateColorGradientStop(0.5f, Color.FromArgb(255, 86, 57, 14)));
+            compositionLinearGradientShadowBrush.ColorStops.Insert(1, m_page_compositor.CreateColorGradientStop(1f, Color.FromArgb(255, 230, 160, 53)));
+            compositionLinearGradientShadowBrush.Offset = new Vector2(0, 4);
+            compositionSpriteShapeShadow.FillBrush = compositionLinearGradientShadowBrush;
+            shapeVisualShadow = m_page_compositor.CreateShapeVisual();
+            shapeVisualShadow.Size = new Vector2(50, 50);
+            shapeVisualShadow.Shapes.Add(compositionSpriteShapeShadow);
+        }
 
-            m_page_compositor.CreateRoundedRectangleGeometry();
+        private void CreateTopCircleButton()
+        {
+            CompositionRoundedRectangleGeometry circleGeometry = m_page_compositor.CreateRoundedRectangleGeometry();
+            circleGeometry.Size = new Vector2(40, 40);
+            circleGeometry.CornerRadius = new Vector2(20, 20);
+            CompositionSpriteShape compositionSpriteShape = m_page_compositor.CreateSpriteShape(circleGeometry);
+            circleGradientBrush = m_page_compositor.CreateRadialGradientBrush();
 
-            
-            
+            // Color animation
+            ColorStop1 = m_page_compositor.CreateColorGradientStop(1, Colors.White);
+            circleGradientBrush.ColorStops.Add(ColorStop1);
+            color1Animation = m_page_compositor.CreateColorKeyFrameAnimation();
+            color1Animation.Duration = TimeSpan.FromSeconds(0.5);
+            color1Animation.InsertKeyFrame(1.0f, Color.FromArgb(255, 247, 211, 156));
+            color1Animation.InsertKeyFrame(0.0f, Colors.White);
+            color2Animation = m_page_compositor.CreateColorKeyFrameAnimation();
+            color2Animation.Duration = TimeSpan.FromSeconds(0.5);
+            color2Animation.InsertKeyFrame(1.0f, Colors.White);
+            color2Animation.InsertKeyFrame(0.0f, Color.FromArgb(255, 247, 211, 156));
+
+            compositionSpriteShape.FillBrush = circleGradientBrush;
+            compositionSpriteShape.Offset = new Vector2(5, 5);
+
+            shapeVisualCircle = m_page_compositor.CreateShapeVisual();
+            shapeVisualCircle.Size = new Vector2(50, 50);
+            shapeVisualCircle.Shapes.Add(compositionSpriteShape);
+            shapeVisualCircle.Offset = new Vector3(m_sliderMargins.X, 0, 0);
+
+            // Return animation
+            _leftoffsetAnimation = m_page_compositor.CreateVector3KeyFrameAnimation();
+            _leftoffsetAnimation.InsertKeyFrame(1.0f, new Vector3(0 + m_sliderMargins.X, 0, 0));
+            _leftoffsetAnimation.Duration = TimeSpan.FromSeconds(0.2f);
+            _rightoffsetAnimation = m_page_compositor.CreateVector3KeyFrameAnimation();
+            _rightoffsetAnimation.InsertKeyFrame(1.0f, new Vector3(100 - m_sliderMargins.Y, 0, 0));
+            _rightoffsetAnimation.Duration = TimeSpan.FromSeconds(0.2f);
+
+        }
+
+        private void createCanvas()
+        {
+            m_page.PointerMoved += M_page_PointerMoved;
+            m_page.PointerPressed += M_page_PointerPressed;
+            visuals = m_page_compositor.CreateContainerVisual();
+            ElementCompositionPreview.SetElementChildVisual(m_page, visuals);
+
+            // Text preparation
+/*            TextBlock text = new TextBlock()
+            {
+                Text = m_switchText,
+                Padding = new Thickness(0, 15, 25, 0),
+                FontWeight = FontWeights.Bold,
+                Height = 50,
+                HorizontalTextAlignment = TextAlignment.Right
+            };
+            Visual textVisual = ElementCompositionPreview.GetElementVisual(text);
+*/
+
+            // ELIPSE
+            CreateMainElipse();
             visuals.Children.InsertAtTop(shapeVisualElipse);
 
+            // Shadow
+            CreateTopCircleButtonShadow();
+            visuals.Children.InsertAtTop(shapeVisualShadow);
+
+            // TEXT
+            //visuals.Children.InsertAtTop(textVisual);
+
+            // CIRCLE
+            CreateTopCircleButton();
+            visuals.Children.InsertAtTop(shapeVisualCircle);
+
+            
+        }
+
+        private void M_page_PointerPressed(object sender, PointerRoutedEventArgs e)
+        {
+            isPressed = true;
+            ColorStop1.StartAnimation(nameof(ColorStop1.Color), color1Animation);
+            M_page_PointerMoved(sender, e);
+        }
+
+        private void M_page_PointerMoved(object sender, PointerRoutedEventArgs e)
+        {
+            if (isPressed == true)
+            {
+                pointerPosition = new Vector3(e.GetCurrentPoint((UIElement)sender).Position.ToVector2().X - 25, 0, 0);
+                if (pointerPosition.X <= 0 + m_sliderMargins.X)
+                    pointerPosition.X = 0 + m_sliderMargins.X;
+                if (pointerPosition.X >= 100 - m_sliderMargins.Y)
+                    pointerPosition.X = 100 - m_sliderMargins.Y;
+                shapeVisualShadow.Offset = pointerPosition;
+                shapeVisualCircle.Offset = pointerPosition;
+            }
         }
 
         ~MyCustomSlider()
