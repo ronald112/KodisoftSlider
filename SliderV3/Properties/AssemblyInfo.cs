b8cacc6aa6aee351e15ca08a83ae77df5cbbc81e
ronald112 <ronald11211@gmail.com>
2020-11-13 21:27:17 +0200

    Add v3

--- SliderV3/Properties/AssemblyInfo.cs
+++ SliderV3/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Slider")]
+[assembly: AssemblyTitle("SliderV3")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Slider")]
+[assembly: AssemblyProduct("SliderV3")]
 [assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
